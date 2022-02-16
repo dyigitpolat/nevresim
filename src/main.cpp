94cd1d560512bf535a18b7ab7f6f519f516b1938
yigit polat <yigit@u.nus.edu>
2022-02-17 01:12:42 +0800

    single core simple test pass

--- src/main.cpp
+++ src/main.cpp
@@ -100,78 +100,161 @@ public:
     }
 };
 
-struct AxonSource
+struct SpikeSource
 {
-    core_id_t core_;
-    axon_id_t axon_;
+    core_id_t core_{};
+    axon_id_t neuron_{};
 };
 
 template<std::size_t AxonCount>
 struct CoreConnection {
-    std::array<AxonSource, AxonCount> sources_{};
+    std::array<SpikeSource, AxonCount> sources_{};
+};
+
+constexpr core_id_t k_input_buffer_id{
+    std::numeric_limits<core_id_t>::max()
 };
 
 template<
-    std::size_t CoreCount, 
     std::size_t AxonCount,
-    std::size_t InputCoreCount,
-    std::size_t OutputCoreCount>
+    std::size_t CoreCount,
+    std::size_t OutputSize>
 struct ChipConfiguration {
     std::array<CoreConnection<AxonCount>, CoreCount> core_sources_{};
-    std::array<core_id_t, InputCoreCount> input_cores_{};
-    std::array<core_id_t, OutputCoreCount> output_cores_{};
+    std::array<SpikeSource, OutputSize> output_sources_{};
 };
+
+template <size_t N> consteval
+auto make_spike_array(auto iter) 
+{
+    return 
+        [&]<std::size_t ...Idx> (std::index_sequence<Idx...>)
+        {
+            return std::array<spike_t, N> { ((void)Idx, *(iter++))... };
+        }(std::make_index_sequence<N>{});
+} 
 
 template<
     std::size_t AxonCount,
     std::size_t NeuronCount,
     std::size_t CoreCount,
+    std::size_t InputSize,
+    std::size_t OutputSize,
     threshold_t Threshold,
-    ChipConfiguration Configuration>
+    ChipConfiguration<
+        AxonCount,
+        CoreCount,
+        OutputSize> Configuration
+    >
 class Chip
 {
     using cores_array_t = 
         std::array< Core< AxonCount, NeuronCount, Threshold>, CoreCount>;
+    
+    using input_buffer_t = 
+        std::array< spike_t, InputSize>;
 
     cores_array_t cores_{};
+    input_buffer_t input_buffer_{};
 
     consteval
-    auto get_input_for(core_id_t core_id)
+    static auto retrieve_spike_from() 
     {
-        auto axon_sources_view = 
-            Configuration.core_sources_[core_id] |
-            std::views::transform([this](auto source){
-                return cores_[source.core_].get_output_spikes()[source.axon_];
-            });
-        
-        return std::vector{
-            std::ranges::begin(axon_sources_view), 
-            std::ranges::end(axon_sources_view)
+        return [](const Chip& chip, SpikeSource source) -> spike_t {
+            if(source.core_ == k_input_buffer_id)
+            {
+                return chip.input_buffer_[source.neuron_];
+            }
+            else
+            {
+                return 
+                    chip.cores_[source.core_]
+                        .get_output_spikes()[source.neuron_];
+            }
+        };
+    }
+
+    template <std::size_t N> consteval
+    static auto retrieve_spikes() 
+    {
+        /*
+        return [](const Chip& chip, auto spike_sources) {
+            constexpr auto spikes_view = 
+                std::ranges::subrange(spike_sources) |
+                std::views::transform(
+                    [chip](auto source)
+                    {  
+                        return retrieve_spike_from()(chip, source);
+                    });
+            
+            return make_spike_array<N>(std::ranges::begin(spikes_view));
+        };
+        */
+        return [](const Chip& chip, auto spike_sources) {
+            std::array<spike_t, N> spikes{};
+
+            [&]<std::size_t ...Idx> (std::index_sequence<Idx...>)
+            {
+                (
+                    (spikes[Idx] 
+                        = retrieve_spike_from()(chip, spike_sources[Idx])
+                    )
+                , ...);
+            }(std::make_index_sequence<N>{});
+
+            return spikes;
+        };
+    }
+
+    consteval
+    static auto get_input_for() 
+    {
+        return [](const Chip& chip, core_id_t core_id){
+            return retrieve_spikes<AxonCount>()(
+                chip, Configuration.core_sources_[core_id].sources_);
         };
     }
 
 public:
     constexpr 
     Chip(cores_array_t cores) : cores_(cores) {}
-
-    constexpr
-    void compute(const auto& incoming_spikes)
+    
+    constexpr 
+    static auto generate_read_output_buffer()
     {
-        [this] <core_id_t ...IDs>
-        (std::index_sequence<IDs...>)
-        {
-            (cores_[IDs].compute(get_input_for(IDs)), ...);
-        } (std::make_index_sequence<CoreCount>{});
+        return [](const Chip& chip){
+            return 
+                retrieve_spikes<OutputSize>()(
+                    chip, Configuration.output_sources_);
+        };
+    }
+
+    consteval 
+    static auto generate_compute() 
+    {
+        return [](Chip& chip){
+            [] <core_id_t ...IDs>
+            (Chip& chip_, std::index_sequence<IDs...>)
+            {
+                (chip_.cores_[IDs].compute(get_input_for()(chip_, IDs)), ...);
+            } (chip, std::make_index_sequence<CoreCount>{});
+        };
+    }
+
+    void feed_input_buffer(const input_buffer_t& feed)
+    {
+        input_buffer_ = feed;
     }
 };
 
 } // namespace nevresim
 
 
-void test_simple_core()
+void test_single_core()
 {
-    nevresim::Core<3,2,10> core{
+    nevresim::Core<3,3,10> core{
         std::array{
+            nevresim::Neuron<3,10>{std::array{1,2,3}},
             nevresim::Neuron<3,10>{std::array{4,5,6}},
             nevresim::Neuron<3,10>{std::array{7,8,9}}}
     };
@@ -186,9 +269,42 @@ void test_simple_core()
     }
 }
 
-void test_simple_chip()
+
+void test_single_core_chip()
 {
-    
+    constexpr nevresim::ChipConfiguration<3,1,3> config {
+        {nevresim::CoreConnection<3>{
+            std::array<nevresim::SpikeSource, 3>{
+                nevresim::SpikeSource{nevresim::k_input_buffer_id, 0},
+                nevresim::SpikeSource{nevresim::k_input_buffer_id, 1},
+                nevresim::SpikeSource{nevresim::k_input_buffer_id, 2}}
+        }},
+        {
+            std::array<nevresim::SpikeSource, 3>{
+                nevresim::SpikeSource{0, 0},
+                nevresim::SpikeSource{0, 1},
+                nevresim::SpikeSource{0, 2}}        
+        }
+    };
+
+    nevresim::Chip<3,3,1,3,3,10, config> chip {
+        std::array<nevresim::Core<3,3,10>, 1> {
+            nevresim::Core<3,3,10> {
+                std::array{
+                    nevresim::Neuron<3,10>{std::array{1,2,3}},
+                    nevresim::Neuron<3,10>{std::array{4,5,6}},
+                    nevresim::Neuron<3,10>{std::array{7,8,9}}}
+                }
+        }
+    };
+
+    std::array<nevresim::spike_t, 3> input{1,1,0};
+    chip.feed_input_buffer(input);
+    nevresim::Chip<3,3,1,3,3,10, config>::generate_compute()(chip);
+    for(auto spike : chip.generate_read_output_buffer()(chip))
+    {
+        std::cout << static_cast<int> (spike);
+    }
 }
 
 int main()
@@ -202,7 +318,8 @@ int main()
     //-----------------------------------------//
 
     DemoMenu main_menu("MAIN MENU");
-    main_menu.add_menu_item({test_simple_core, "test simple core"});
+    main_menu.add_menu_item({test_single_core, "test single core"});
+    main_menu.add_menu_item({test_single_core_chip, "test single core chip"});
     main_menu.show();
 
     return 0;
