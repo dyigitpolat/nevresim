084712288150a4394de25031c13f9b3df76b1d40
yigit polat <yigit@u.nus.edu>
2022-02-17 14:26:44 +0800

    null connection support

--- include/constants.hpp
+++ include/constants.hpp
@@ -10,4 +10,8 @@ constexpr core_id_t k_input_buffer_id{
     std::numeric_limits<core_id_t>::max()
 };
 
+constexpr core_id_t k_no_connection{
+    std::numeric_limits<core_id_t>::max() - 1
+};
+
 } // namespace nevresim
