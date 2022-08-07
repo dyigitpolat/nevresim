66275602f81d2d866bb736737ff87ecdfed5e681
dyigitpolat <yigit@u.nus.edu>
2022-08-07 18:57:19 +0800

    delete older types

--- include/types.hpp
+++ include/types.hpp
@@ -5,11 +5,7 @@
 namespace nevresim {
 
 using raw_input_t = double;
-
 using weight_t = double;
-using threshold_t = weight_t;
-using membrane_potential_t = threshold_t;
-using membrane_leak_t = membrane_potential_t;
 using spike_t = std::int_fast8_t;
 
 using core_id_t = std::size_t;
