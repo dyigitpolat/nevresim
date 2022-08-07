03e5024f63870803278a65bd15a1d7bc464830e4
dyigitpolat <yigit@u.nus.edu>
2022-08-07 18:14:34 +0800

    customizable type definitions for the chip

--- include/types.hpp
+++ include/types.hpp
@@ -15,4 +15,29 @@ using spike_t = std::int_fast8_t;
 using core_id_t = std::size_t;
 using axon_id_t = std::size_t;
 
+template <typename WeightType>
+struct ChipTypes
+{
+    using weight_t = WeightType;
+    using threshold_t = weight_t;
+    using membrane_potential_t = threshold_t;
+    using membrane_leak_t = membrane_potential_t;
+};
+
+template <typename WeightType>
+using chip_weight_t = 
+    typename ChipTypes<WeightType>::weight_t;
+
+template <typename WeightType>
+using chip_threshold_t = 
+    typename ChipTypes<WeightType>::threshold_t;
+
+template <typename WeightType>
+using chip_membrane_potential_t = 
+    typename ChipTypes<WeightType>::membrane_potential_t;
+
+template <typename WeightType>
+using chip_membrane_leak_t = 
+    typename ChipTypes<WeightType>::membrane_leak_t;
+
 } // namespace nevresim
