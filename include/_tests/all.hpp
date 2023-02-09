f0ace9072c2f1b354be683f2d0af1efffd403895
dyigitpolat <yigit@u.nus.edu>
2023-02-09 18:36:41 +0800

    get rid of redundant code

--- include/_tests/all.hpp
+++ include/_tests/all.hpp
@@ -7,6 +7,8 @@
 #include "_tests/real_valued_multi_core.hpp"
 #include "_tests/xor_test/xor_test.hpp"
 
+#include "simulator/compute_policy/fire_policy/novena_fire.hpp"
+
 #include <iostream>
 
 namespace nevresim
@@ -25,8 +27,8 @@ void run_all()
     report_result("3 core 2x2", test_3_core_2x2);
     report_result("real valued single core", test_single_real_valued_core);
     report_result("real valued multi core", test_multi_real_valued_core);
-    report_result("xor test", test_xor);
-    report_result("xor test novena fire", test_xor_novena);
+    report_result("xor test", test_xor<>);
+    report_result("xor test novena fire", test_xor<NovenaFirePolicy>);
 }
 
 } // namespace nevresim::tests
--- include/_tests/xor_test/xor_test.hpp
+++ include/_tests/xor_test/xor_test.hpp
@@ -4,7 +4,7 @@
 #include "simulator/execution/spiking_execution.hpp"
 #include "simulator/compute_policy/spiking_compute.hpp"
 #include "simulator/compute_policy/real_valued_compute.hpp"
-#include "simulator/compute_policy/fire_policy/novena_fire.hpp"
+#include "simulator/compute_policy/fire_policy/default_fire.hpp"
 
 #include "_tests/test_util.hpp"
 #include "_tests/xor_test/xor_chip.hpp"
@@ -14,28 +14,13 @@
 namespace nevresim::tests
 {
 
+template <typename FirePolicy = DefaultFirePolicy>
 bool test_xor()
 {
     static constinit auto chip = 
-        generate_xor_chip<SpikingCompute<>>();
+        generate_xor_chip<SpikingCompute<FirePolicy>>();
     
-    using spiking_1000 = SpikingExecution<1000, StochasticSpikeGenerator>;
-
-    load_weights(
-        chip, "include/_tests/xor_test/xor_chip_weights.txt");
-
-    auto [correct, total] = test_on_inputs<spiking_1000>(
-        chip,"include/_tests/xor_test/xor_inputs/xor_input_", 4);
-
-    return (correct == total);
-}
-
-bool test_xor_novena()
-{
-    static constinit auto chip = 
-        generate_xor_chip<SpikingCompute<NovenaFirePolicy>>();
-    
-    using spiking_1000 = SpikingExecution<1000, StochasticSpikeGenerator, NovenaFirePolicy>;
+    using spiking_1000 = SpikingExecution<1000, StochasticSpikeGenerator, FirePolicy>;
 
     load_weights(
         chip, "include/_tests/xor_test/xor_chip_weights.txt");
